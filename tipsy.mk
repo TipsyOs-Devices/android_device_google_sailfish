bd95747c4661c392b738f75ded452a3f38a3c270
f100cleveland <dustin.rinne@gmail.com>
2017-02-19 22:10:56 -0600

    Update and rename carbon_sailfish.mk to tipsy.mk

--- tipsy.mk
+++ tipsy.mk
@@ -2,19 +2,19 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/tipsy/config/common.mk)
+
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Inherit aosp_sailfish
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 
--include device/google/marlin/sailfish/device-carbon.mk
+-include device/google/marlin/sailfish/device-tipsy.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := carbon_sailfish
+PRODUCT_NAME := tipsy_sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := Google
@@ -24,8 +24,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
     BUILD_FINGERPRINT=google/sailfish/sailfish:7.1.1/NOF26W/3637559:user/release-keys \
     PRIVATE_BUILD_DESC="sailfish-user 7.1.1 NOF26W 3637559 release-keys"
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="Myself5"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
